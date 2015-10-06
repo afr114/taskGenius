9bb4acf0509a2da9bf3113d21124674919dd5fa9
abdul rahimi <afr114@gmail.com>
2015-10-05 19:53:05 -0700

    add landing page with bootstrap

--- app/models/task.js
+++ app/models/task.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   description: DS.attr(),
   status: DS.attr(),
-  notes: DS.attr(),
+  note: DS.attr(),
   priority: DS.attr(),
   goal: DS.belongsTo('goal', {async:true}),
   steps: DS.hasMany('step', {async: true})
