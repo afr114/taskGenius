3f254f7f3e1c364cd4ad39013704f9724fdfd883
Abdul Rahimi <afr114@gmail.com>
2015-10-07 18:19:06 -0700

    add forms

--- app/components/new-list.js
+++ app/components/new-list.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-});
+  actions: {
+    saveList() {
+      var params = {
+        name: this.get('name'),
+        };
+      this.sendAction('saveList', params);
+      debugger;
+      }
+    }
+  });
--- app/routes/listtile.js
+++ app/routes/listtile.js
@@ -3,6 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('list');
-  }
+  },
 
+  actions: {
+    saveList(params) {
+      var newList = this.store.createRecord('list', params);
+      newList.save();
+      this.transitionTo('listtile')
+    }
+  }
 });
